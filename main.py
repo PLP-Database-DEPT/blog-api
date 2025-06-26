9db4043903a249831cb2d657f05a554bbd35e130
PLP-Database-DEPT <zablon@powerlearnprojectafrica.org>
2025-06-26 05:32:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from src.db import initdb
+from src.routes import router as blog_router  
 
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await initdb() 
+    yield
+
+app = FastAPI(
+    title="Blog API",
+    description="A simple RESTful API for software testing class",
+    lifespan=lifespan
+)
+
+app.include_router(blog_router)  
+
+
+@app.get("/")
+def home():
+    return  {"msg": "Hello World"}
